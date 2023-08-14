54d1d205a130ea241bc9a6af34926fca9afaf444
Callay <callaysun@gmail.com>
2023-08-14 23:25:15 +0800

    to drive2

--- exercises/drive/drive2.rs
+++ exercises/drive/drive2.rs
@@ -3,8 +3,9 @@
 // Execute `rustlings hint drive1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+
+use std::ptr;
 
 struct Foo {
     a: u128,
@@ -15,6 +16,11 @@ fn raw_pointer_to_box(address: usize) -> Box<Foo> {
     // address is a pointer that points to heap.
     // construct Box from this address, and modify Foo's b field to 
     // the string "hello"
+    unsafe{
+        let mut raw_box=Box::from_raw(address as *mut Foo);
+        raw_box.b=Some("hello".to_string());
+        raw_box
+    }
 }
 
 
